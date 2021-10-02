8436d04bb3b83e0b1cfc9b418f8bdfeb5a60ac89
Kleyton Solinho <kleytonsolinho@gmail.com>
2021-10-02 09:57:00 -0300

    home section video

--- src/components/HomeComponents/Video/index.jsx
+++ src/components/HomeComponents/Video/index.jsx
@@ -1,7 +1,19 @@
+import lightningIcon from '../../../assets/images/site/thunder-lightning-circle.svg';
+
 import { Container } from './styles';
 
 export default function Video() {
   return (
-    <Container>Video</Container>
+    <Container>
+      <p>
+        Mega investidor e bestseller
+        <img src={lightningIcon} alt="icon" />
+      </p>
+      <h1>
+        <strong>Lançamento </strong>
+        Howard Marks
+      </h1>
+      <iframe src="https://www.youtube.com/embed/M0juBlRCJNc" title="Lançamento Howard Marks" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+    </Container>
   );
 }
