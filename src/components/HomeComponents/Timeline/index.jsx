73099479bb4671d5af9173ba7cf975f64f3ad3a6
Kleyton Solinho <kleytonsolinho@gmail.com>
2021-10-03 00:11:24 -0300

    add home section timeline trail

--- src/components/HomeComponents/Timeline/index.jsx
+++ src/components/HomeComponents/Timeline/index.jsx
@@ -1,9 +1,110 @@
-import { Container } from './styles';
+import iconSchool from '../../../assets/images/site/icon-school.svg';
+import iconUser from '../../../assets/images/site/user-icon-prof.svg';
+import iconButton from '../../../assets/images/site/begg.svg';
+
+import grupoIcon0 from '../../../assets/images/site/Group_0.svg';
+import h1Line from '../../../assets/images/site/lhl.svg';
+import circleIcon from '../../../assets/images/site/circle.svg';
+
+import {
+  Container, Header, User, SelectTrail, TrailContent,
+} from './styles';
 
 export default function Timeline() {
   return (
     <Container>
-      <h1>Timeline</h1>
+      <Header>
+        <img src={iconSchool} alt="Trilha de conhecimento" />
+        <h1>
+          <span>O que </span>
+          você irá
+          <br />
+          aprender na Finclass
+        </h1>
+        <p>Você evolui junto com a gente</p>
+      </Header>
+      <User>
+        <img src={iconUser} alt="Trilha de conhecimento" />
+        <hr />
+      </User>
+      <SelectTrail>
+        <div className="btns">
+          <button type="button" className="active-btn">
+            <img src={iconButton} alt="" />
+            Iniciante
+          </button>
+          <button type="button">
+            <img src={iconButton} alt="" />
+            Intermediário
+          </button>
+          <button type="button">
+            <img src={iconButton} alt="" />
+            Avançado
+          </button>
+        </div>
+        <div className="begginer active-b" />
+        <div className="advanced" />
+      </SelectTrail>
+      <TrailContent>
+        <ul>
+          <li className="left">
+            <img src={grupoIcon0} alt="" className="icon" />
+            <div className="description">
+              <h1>Aprenda do zero a investir</h1>
+              <p>
+                Tenha conteúdos introdutórios para
+                <br />
+                você começar a investir do jeito certo.
+              </p>
+            </div>
+            <img src={h1Line} alt="" />
+            <img src={circleIcon} alt="" />
+          </li>
+          <li className="right" />
+          <li className="left" />
+          <li className="right">
+            <img src={circleIcon} alt="" />
+            <img src={h1Line} alt="" />
+            <img src={grupoIcon0} alt="" className="icon" />
+            <div className="description">
+              <h1>Aprenda do zero a investir</h1>
+              <p>
+                Tenha conteúdos introdutórios para
+                <br />
+                você começar a investir do jeito certo.
+              </p>
+            </div>
+          </li>
+          <li className="left">
+            <img src={grupoIcon0} alt="" className="icon" />
+            <div className="description">
+              <h1>Aprenda do zero a investir</h1>
+              <p>
+                Tenha conteúdos introdutórios para
+                <br />
+                você começar a investir do jeito certo.
+              </p>
+            </div>
+            <img src={h1Line} alt="" />
+            <img src={circleIcon} alt="" />
+          </li>
+          <li className="right" />
+          <li className="left" />
+          <li className="right">
+            <img src={circleIcon} alt="" />
+            <img src={h1Line} alt="" />
+            <img src={grupoIcon0} alt="" className="icon" />
+            <div className="description">
+              <h1>Aprenda do zero a investir</h1>
+              <p>
+                Tenha conteúdos introdutórios para
+                <br />
+                você começar a investir do jeito certo.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </TrailContent>
     </Container>
   );
 }
--- src/components/HomeComponents/Timeline/styles.js
+++ src/components/HomeComponents/Timeline/styles.js
@@ -1,5 +1,203 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  max-width: 1280px;
+  height: auto;
+  padding: 5rem 2rem;
+  width: 100%;
+  margin: 0 auto;
 
+  .active-btn {
+    border: 1px solid #00e7f9 !important;
+  }
+
+  .active-b {
+    border-right: 1px solid #00e7f922;
+    border-top: 1px solid #00e7f9;
+    border-left: 1px solid #00e7f9;
+    border-bottom: 1px solid #00e7f9;
+  }
+
+  .active-a {
+    border-left: 1px solid #00e7f922;
+    border-top: 1px solid #00e7f9;
+    border-right: 1px solid #00e7f9;
+    border-bottom: 1px solid #00e7f9;
+  }
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+
+  span {
+    color: #86888d;
+  }
+
+  h1 {
+    margin: 1rem 0;
+  }
+
+  p {
+    opacity: 0.7;
+    font-weight: 300;
+  }
+`;
+
+export const User = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 4rem;
+
+  hr {
+    border-top: 1px solid #00e7f9;
+    width: 43px;
+    transform: rotate(90deg);
+    margin-top: 1rem;
+  }
+`;
+
+export const SelectTrail = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 22px;
+
+  .btns {
+    width: 100%;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    top: 40px;
+    z-index: 2;
+
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+
+      border: 1px solid #0f1116;
+      width: 290px;
+      border-radius: 14.5px;
+      background: #0f1116;
+      padding: 2rem;
+      font-size: 1.5rem;
+      line-height: 27px;
+      color: #ffffff;
+      cursor: pointer;
+    }
+  }
+
+  .begginer {
+    width: 500px;
+    height: 250px;
+    position: relative;
+    background: transparent;
+  }
+
+  .advanced {
+    width: 500px;
+    height: 250px;
+    position: relative;
+    background: transparent;
+  }
+`;
+
+export const TrailContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ul {
+    list-style: none;
+    position: relative;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    margin: 0 auto;
+
+    .left {
+      width: 100%;
+      height: 180px;
+      position: relative;
+
+      display: flex;
+      align-items: center;
+      padding: 5rem 0 3rem 0;
+
+      border-right: 1px solid #00e7f9;
+
+      .icon {
+        margin-right: 2rem;
+      }
+
+      .description {
+        h1 {
+          font-size: 1.2rem;
+        }
+
+        p {
+          font-size: 0.9rem;
+          font-weight: 300;
+          margin-top: 1rem;
+        }
+      }
+
+      img:nth-of-type(2) {
+        width: 252px;
+        height: unset;
+      }
+
+      img:nth-of-type(3) {
+        width: 40px;
+        height: 40px;
+        position: absolute;
+        right: -20px;
+      }
+    }
+
+    .right {
+      width: 100%;
+      height: 180px;
+      position: relative;
+
+      display: flex;
+      align-items: center;
+      padding: 5rem 0 3rem 0;
+
+      img:nth-of-type(2) {
+        width: 252px;
+        height: unset;
+      }
+
+      img:nth-of-type(1) {
+        width: 40px;
+        height: 40px;
+        position: absolute;
+        left: -20px;
+      }
+
+      .icon {
+        margin-right: 2rem;
+      }
+
+      .description {
+        h1 {
+          font-size: 1.2rem;
+        }
+
+        p {
+          font-size: 0.9rem;
+          font-weight: 300;
+          margin-top: 1rem;
+        }
+      }
+    }
+  }
 `;
